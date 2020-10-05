b23a920a042f5695411c2c96bdcb5efb6e67694b
yungpowerade <51759082+yungpowerade@users.noreply.github.com>
2020-10-05 17:05:08 -0400

    Changed the Variable Names

--- SSW555Project02FINALVERSION/main.py
+++ SSW555Project02FINALVERSION/main.py
@@ -1,4 +1,4 @@
-
+import python-gedcom
 key_words = ['INDI','NAME','SEX','BIRT','DEAT','FAMC','FAMS','FAM',
 'MARR','HUSB','WIFE','CHIL','DIV','DATE','HEAD','TRLR','NOTE']
 
@@ -10,22 +10,22 @@ text_file = open('export-BloodTree.ged', 'r')
 
 
 y = '000'
-lerp = '000'
-derp = '000'
+numberPriority = '000'
+tag = '000'
 for line in text_file:
   level_number = line[:1]
-  
+
   line = line.split()
 
   for word in key_words:
       if word in line:
-       
-       derp = word
+
+       tag = word
        y = 'Y'
 
-  for burb in levels:
-      if burb in line:
-        lerp = burb
+  for level in levels:
+      if level in line:
+        numberPriority = level
 
-  print('<-- |' + derp + '|' + lerp + '|' + y)
+  print('<-- |' + tag + '|' + numberPriority + '|' + y)
   print("-->", line)
